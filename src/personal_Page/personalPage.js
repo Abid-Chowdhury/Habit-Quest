157f7dbb8529038bcff8ae1d2877e5f278200b63
SininAssets <ilia.sinin@gmail.com>
2024-04-21 12:36:28 -0700

    Fixed Up Personal Page

--- src/personal_Page/personalPage.js
+++ src/personal_Page/personalPage.js
@@ -1,3 +1,3 @@
 var button = document.querySelector('.exit_Button');
 button.addEventListener('click',function(){
-    window.location.href = '../home_Page/home_Page.html';})
+    window.location.href = '../home_Page_2/home_Page_2.html';})
