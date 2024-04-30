8ee12e80e137602f35a7ecd4c9bbde455d3f2f83
Abid-Chowdhury <mr.abid2004@gmail.com>
2024-04-30 00:47:06 -0400

    Added JS code

--- src/homePage3/homePage3.js
+++ src/homePage3/homePage3.js
@@ -1,63 +1,63 @@
-// const timeElement = document.querySelector(".time");
-// const dateElement = document.querySelector(".date");
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
 
-// /**
-//  * @param {Date} date
-//  */
-// function timeFormat(date) {
-//     const hours = date.getHours() % 12 || 12;
-//   const minutes = date.getMinutes();
-//   const isAm = date.getHours() < 12;
+/**
+ * @param {Date} date
+ */
+function timeFormat(date) {
+    const hours = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const isAm = date.getHours() < 12;
 
-//   return `${hours.toString().padStart(2, "0")}:${minutes
-//     .toString()
-//     .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
-// }
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+}
 
-// /**
-//  * @param {Date} date
-//  */
-// function dateFormat(date) {
-//   const DAYS = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-//   ];
-//   const MONTHS = [
-//     "Jan",
-//     "Feb",
-//     "Mar",
-//     "Apr",
-//     "May",
-//     "Jun",
-//     "Jul",
-//     "Aug",
-//     "Sept",
-//     "Oct",
-//     "Nov",
-//     "Dec"
-//   ];
+/**
+ * @param {Date} date
+ */
+function dateFormat(date) {
+  const DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
 
-//   return `${DAYS[date.getDay()]}, ${
-//     MONTHS[date.getMonth()]
-//   } ${date.getDate()} ${date.getFullYear()}`;
-// }
+  return `${DAYS[date.getDay()]}, ${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()} ${date.getFullYear()}`;
+}
 
-// setInterval(() => {
-//   const now = new Date();
+setInterval(() => {
+  const now = new Date();
 
-//   timeElement.textContent = timeFormat(now);
-//   dateElement.textContent = dateFormat(now);
-// }, 200);
+  timeElement.textContent = timeFormat(now);
+  dateElement.textContent = dateFormat(now);
+}, 200);
 
-// const profileButton = document.getElementById("profileButton");
-// const profileSidebar = document.getElementById("profileSidebar");
+const profileButton = document.getElementById("profileButton");
+const profileSidebar = document.getElementById("profileSidebar");
 
-// profileButton.addEventListener('click',function(){
-//   profileSidebar.classList.toggle("show")
-// });
+profileButton.addEventListener('click',function(){
+  profileSidebar.classList.toggle("show")
+});
 
