71a46ed8c79f929cdfb20ab9ae7208a1dc049678
Abid-Chowdhury <mr.abid2004@gmail.com>
2024-06-09 02:25:10 -0400

    Only show scrollbar when tasks are overflowing

--- src/homePage3/homePage3.js
+++ src/homePage3/homePage3.js
@@ -65,3 +65,70 @@ function updateDate() {
 
 updateDate()
 setInterval(updateDate, 1000)
+
+
+
+
+
+
+
+
+
+
+// CHECK FOR TASKS OVERFLOW
+// Determines if the passed element is overflowing its bounds,
+// either vertically or horizontally.
+// Will temporarily modify the "overflow" style to detect this
+// if necessary.
+function checkOverflow(elementBeingChecked)
+{
+   var curOverflow = elementBeingChecked.style.overflow;
+
+   if ( !curOverflow || curOverflow === "visible" )
+      elementBeingChecked.style.overflow = "hidden";
+
+   var isOverflowing = elementBeingChecked.clientWidth < elementBeingChecked.scrollWidth 
+      || elementBeingChecked.clientHeight < elementBeingChecked.scrollHeight;
+
+   elementBeingChecked.style.overflow = curOverflow;
+
+   return isOverflowing;
+}
+
+var elm = document.getElementById('habits-task-list')
+if (!checkOverflow(elm)) {
+    console.log('no overflow')
+    elm.classList.remove('show')
+    elm.classList.add('hide')
+    
+} else {
+    console.log('overflow')
+    elm.classList.remove('hide')
+    elm.classList.add('show')
+
+}
+
+var elm = document.getElementById('daily-task-list')
+if (!checkOverflow(elm)) {
+    console.log('no overflow')
+    elm.classList.remove('show')
+    elm.classList.add('hide')
+    
+} else {
+    console.log('overflow')
+    elm.classList.remove('hide')
+    elm.classList.add('show')
+
+}
+var elm = document.getElementById('todo-task-list')
+if (!checkOverflow(elm)) {
+    console.log('no overflow')
+    elm.classList.remove('show')
+    elm.classList.add('hide')
+    
+} else {
+    console.log('overflow')
+    elm.classList.remove('hide')
+    elm.classList.add('show')
+
+}
